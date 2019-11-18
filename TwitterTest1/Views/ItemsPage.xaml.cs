13d698adf730d25e8292673a2446b4a655cc64b0
sbkrishnan2506 <srinivasan.balakrishnan.contractor@pepsico.com>
2019-11-17 20:00:17 -0500

    Initial project checkin

---
+++ TwitterTest1/Views/ItemsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TwitterTest1.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        public ItemsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
